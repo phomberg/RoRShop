32bd8db96f6d04fe8ae013978960c620742fd910
unknown <RA@.(none)>
2011-03-08 16:00:59 +0100

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRShop::Application.config.secret_token = '523fc0f7d28ff526a24d0213b591564520d17da50f9e7eeaf5ca61943ea885912bc9464b26b04df77a36ef2068dd1de02e4c19f07c8792e155d9bcffa63dd062'
